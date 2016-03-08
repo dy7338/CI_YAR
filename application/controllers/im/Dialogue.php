1c33abf419041238fd71a2078ddcc38a62969b47
Chiang <chiangzg@gmail.com>
2016-03-08 14:25:04 +0800

    delete xmpphp

--- application/controllers/im/Dialogue.php
+++ application/controllers/im/Dialogue.php
@@ -1,42 +1,17 @@
 <?php
 use iface\base\controller;
-use libraries\Xmpphp;
-
+require_once APPPATH . 'libraries';
 /**
- * xmpphp,创建会话.
+ * xmpp,创建会话.
  * User: chiang
  * Date: 16-3-7
  * Time: 下午10:29
  */
 class Dialogue extends MY_controller implements controller
 {
-    private $obj;
     public function __construct()
     {
         parent::__construct();
-        //是实例化一个xmpphp对象
-        $this->_connect();
-    }
-
-    /**
-     * 链接服务器
-     * @param string host
-     * @param int port
-     * @param string username
-     * @param string password
-     * @param string resource
-     * @param string server
-     */
-    private function _connect()
-    {
-        $this->obj = new Xmpphp\XMPPHP_XMPP(
-            '45.78.32.254',
-            5222,
-            'test1',
-            'test1234',
-            'xmpphp',
-            '45.78.32.254'
-        );
-        var_dump($this->_connect());
+//        $this->_connect();
     }
 }
